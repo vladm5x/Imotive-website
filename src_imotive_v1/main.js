1867c5555160b6435983be4e3bf01dcdd83bbac8
vladm5x <muresanvlad123@gmail.com>
2026-04-28 21:47:29 +0200

    Data cleaning try 1

--- src_imotive_v1/main.js
+++
@@ -1,5 +0,0 @@
-import React from "https://esm.sh/react@18.2.0";
-import { createRoot } from "https://esm.sh/react-dom@18.2.0/client";
-import { App } from "./components/App.js";
-
-createRoot(document.getElementById("root")).render(React.createElement(App));
